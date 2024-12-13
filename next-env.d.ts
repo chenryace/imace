19c938d9ee8c5453b95e80e58fa67e01ef54cb95
chenryace <78716224+chenryace@users.noreply.github.com>
2024-12-13 09:01:24 +0800

    Update next-env.d.ts

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,30 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
 
-declare module '@aws-sdk/client-s3' {
-  export * from '@aws-sdk/client-s3'
+// 为 next/server 添加类型定义
+declare module 'next/server' {
+  import { NextResponse as OriginalNextResponse } from 'next'
+  export const NextResponse: typeof OriginalNextResponse
 }
 
-declare module 'next/server' {
-  export * from 'next/server'
+// 为 @aws-sdk/client-s3 添加类型定义
+declare module '@aws-sdk/client-s3' {
+  export class S3Client {
+    constructor(config: any)
+    send(command: any): Promise<any>
+  }
+
+  export class PutObjectCommand {
+    constructor(input: any)
+  }
+
+  export class ListObjectsV2Command {
+    constructor(input: any)
+  }
+}
+
+// 确保 React 组件类型正确
+declare module '*.tsx' {
+  const content: React.FC<any> | React.ComponentType<any>
+  export default content
 }
