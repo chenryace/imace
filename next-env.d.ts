18fe3710185e85313524818da91f308cb8acdc9d
chenryace <78716224+chenryace@users.noreply.github.com>
2024-12-13 08:54:26 +0800

    Create next-env.d.ts

---
+++ next-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="next" />
+/// <reference types="next/image-types/global" />
+
+declare module '@aws-sdk/client-s3' {
+  export * from '@aws-sdk/client-s3'
+}
+
+declare module 'next/server' {
+  export * from 'next/server'
+}
