6767d4406e834abd088510c200ecbcb71fc4ccc8
chenryace <78716224+chenryace@users.noreply.github.com>
2024-12-13 08:32:35 +0800

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  // 移除 swcMinify 配置，因为它在新版本中已经不需要了
 }
 
-export default nextConfig 
+export default nextConfig
